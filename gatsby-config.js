c0420c0b3029804102bb21c94a2dd2ad6ecfdac3
Ben James <benjames1999@hotmail.co.uk>
2020-09-18 22:04:11 +0100

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#4D143B`,
+        theme_color: `#4D143B`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
