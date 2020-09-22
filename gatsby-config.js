69f492a2245a7ba924f6efa97fce7544b833f0f3
Ben James <benjames1999@hotmail.co.uk>
2020-09-22 19:37:49 +0100

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-178629664-1",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
