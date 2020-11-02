458c3697f134743b6a0ff5feb03388a161600790
Ben James <benjames1999@hotmail.co.uk>
2020-11-02 17:15:02 +0000

    Render email after mount for less scraping

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,26 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import { Card } from "../Card"
 import { Email, ContactText, ProfileSVG, ProfilesWrapper } from "./styles"
 import GitHubSVG from "../../assets/github.svg"
 import LinkedinSVG from "../../assets/linkedin.svg"
 
-export default () => (
-  <Card cardColor="cardMaroon">
-    <Email href="mailto:ben@benjames.io">ben@benjames.io</Email>
+export default () => {
+  const [email, setEmail] = useState(0)
 
-    <ProfilesWrapper>
-      <ProfileSVG target="_blank" href="https://github.com/bengineer19">
-        <GitHubSVG />
-      </ProfileSVG>
-      <ProfileSVG
-        target="_blank"
-        href="https://www.linkedin.com/in/bengineer19/"
-      >
-        <LinkedinSVG />
-      </ProfileSVG>
-    </ProfilesWrapper>
+  // Prevent email being rendered immediately, to prevent some scrapers
+  useEffect(() => {
+    setEmail("ben@benjames.io")
+  }, [])
 
-    <ContactText>Don't be a stranger - I'd love to chat</ContactText>
-  </Card>
-)
+  return (
+    <Card cardColor="cardMaroon">
+      <Email href={`mailto:${email}`}>{email}</Email>
+
+      <ProfilesWrapper>
+        <ProfileSVG target="_blank" href="https://github.com/bengineer19">
+          <GitHubSVG />
+        </ProfileSVG>
+        <ProfileSVG
+          target="_blank"
+          href="https://www.linkedin.com/in/bengineer19/"
+        >
+          <LinkedinSVG />
+        </ProfileSVG>
+      </ProfilesWrapper>
+
+      <ContactText>Don't be a stranger - I'd love to chat</ContactText>
+    </Card>
+  )
+}
