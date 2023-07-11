efaad47665c408d97aea3b67437c736e7b5a2c1b
bengineer19 <benjames1999@hotmail.co.uk>
2023-07-11 20:09:59 +0100

    Update packages & move GA to Gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,24 @@ module.exports = {
     {
       resolve: `gatsby-plugin-react-helmet`,
     },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "UA-178629664-1",
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingId: "UA-178629664-1",
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-NKZE1G9JH0", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
       },
     },
     {
