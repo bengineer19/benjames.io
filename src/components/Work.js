584d0d69bb46c385febeccbcb0660f4a07cc9ca0
bengineer19 <benjames1999@hotmail.co.uk>
2024-02-11 16:40:33 +0000

    Fix card import

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,6 +6,7 @@ import WorldfundCard from "./WorkCards/Worldfund"
 import AxleCard from "./WorkCards/Axle"
 import OctopusCard from "./WorkCards/Octopus"
 import ArmCard from "./WorkCards/Arm"
+import OthersCard from "./WorkCards/Others/Others"
 import CcCard from "./WorkCards/Cc"
 import InClimateCard from "./WorkCards/InClimate"
 
